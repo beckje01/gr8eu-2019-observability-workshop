d312b13e20304d587cdcf8166010d55caabe2bbb
Jeff Beck <beckje01@gmail.com>
2019-05-27 18:34:26 -0500

    Fix Kibania setup

--- infra/kibana/create-index.sh
+++ infra/kibana/create-index.sh
@@ -2,11 +2,6 @@
 
 set -xeuo pipefail
 
-if ! curl --retry 5 --retry-connrefused --retry-delay 0 -sf http://kibana:5601/api/saved_objects/_find?type=index-pattern; then
-    curl -XPOST -D- 'http://kibana:5601/api/saved_objects/index-pattern' \
-			-H 'Content-Type: application/json' \
-			-H 'kbn-version: 6.6.1' \
-			-d '{"attributes":{"title":"logstash-*","timeFieldName":"@timestamp"}}'
-fi
+curl --retry 10 -XPOST -D- 'http://kibana:5601/api/saved_objects/index-pattern' -H 'Content-Type: application/json' -H 'kbn-version: 6.6.1' -d '{"attributes":{"title":"logstash-*","timeFieldName":"@timestamp"}}'
 
 echo 'Created Index'
